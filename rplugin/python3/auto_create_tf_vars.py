ac52caf02a3a31513c3877d2e9b3acac6b1bce9e
Vuong <tranvuong0402@gmail.com>
2022-08-30 11:47:41 +0700

    Create function to create var for tf

---
+++ rplugin/python3/auto_create_tf_vars.py
@@ -0,0 +1,35 @@
+import pathlib
+import pynvim
+
+@pynvim.plugin
+class TestPlugin:
+    def __init__(self, nvim):
+        self.nvim = nvim
+
+    @pynvim.function("CreateTfVarFunction", sync=True)
+    def testcommand(self, args):
+        current_dir = self.nvim.command_output("pwd")
+        tfvars_file_loc = current_dir + "/variables.tf"
+        if not pathlib.Path(tfvars_file_loc).exists():
+            open(tfvars_file_loc, mode="w", encoding="utf-8").close()
+
+        variable = ""
+        description = ""
+        if len(args) >= 1:
+            variable = args[0]
+        if len(args) >= 2:
+            description = args[1]
+        with open(tfvars_file_loc, mode="r+", encoding="utf-8") as f:
+            content = f.read()
+            var_conditions = ['variable "{variable}"', variable]
+            if not any(map(lambda x: x in content, var_conditions)):
+                template = 'variable "{variable}" {{}}\n'.format(variable=variable)
+                if description:
+                    template = """
+variable "{variable}" {{
+    description = "{description}"
+}}
+""".format(
+                        variable=variable, description=description
+                    )
+                f.write(template)
