0059c4a4edf32c2ec8e9a620d09f28e0ce5d5a62
Vuong <tranvuong0402@gmail.com>
2022-09-14 14:35:22 +0700

    Update new function: TfExample for aws

--- plugin/tfutils.lua
+++ plugin/tfutils.lua
@@ -1,2 +1,3 @@
- vim.api.nvim_set_keymap("", "<leader>cv", ":call TfCreateVar(", {})
- vim.api.nvim_set_keymap("", "<leader>bv", 'yaw :call TfViewDoc("<c-r>"")<cr>', {})
+ vim.api.nvim_set_keymap("", "<leader>tcv", ":call TfCreateVar(", {})
+ vim.api.nvim_set_keymap("", "<leader>tvd", 'yaw :call TfViewDoc("<c-r>"")<cr>', {})
+ vim.api.nvim_set_keymap("", "<leader>tvx", 'yaw :call TfExample("<c-r>"")<cr>', {})
--- rplugin/python3/tfutils.py
+++ rplugin/python3/tfutils.py
@@ -3,6 +3,8 @@ import re
 import pynvim
 import webbrowser
 import requests
+import math
+import logging
 
 
 @pynvim.plugin
@@ -50,36 +52,78 @@ variable "{variable}" {{
         for provider in supported_providers:
             result = re.findall(supported_providers[provider]["regex"], resource_name)
             if result:
-                return supported_providers[provider]["url"] + result[0]
+                return result[0], supported_providers[provider]["url"] + result[0]
         return None
 
     @pynvim.function("TfViewDoc", sync=True)
     def view_doc(self, args):
         """Browse doc base on the resource name"""
         resource_name = args[0]
-        url = self.get_provider_url(resource_name)
+        _, url = self.get_provider_url(resource_name)
         self.nvim.out_write("url")
         webbrowser.open(url)
 
     @pynvim.function("TfExample", sync=True)
     def view_example_doc(self, args):
         resource_name = args[0]
-        resource_url = self._get_resource_url(resource_name)
+        resource_short_name, url = self.get_provider_url(resource_name)
+        resource_url = self._get_resource_url(resource_short_name)
 
-        registry_url = "https://registry.terraform.io"
-        res = requests.get(registry_url + resource_url)
-        #TODO: Filter response data to get example string
+        r = requests.get(resource_url)
+        content = r.json()['data']['attributes']['content']
+        regex_tf = '```terraform(.*?)```'
+        example_data = re.findall(regex_tf, content, re.DOTALL)
+
+        buf = self._show_example_windows()
+        self._update_example_windows(buf, resource_name, example_data)
+
+    def _update_example_windows(self, buf, resource, data):
+        api = self.nvim.api
+        api.buf_set_option(buf, 'modifiable', True)
+        # lines = [line + "\n============\n" for line in data]
+        lines = [line.split("\n") for line in data]
+        api.buf_set_lines(buf, 0, -1, False, [f"Examples from {resource} offcial docs"])
+        for example in lines:
+            api.buf_set_lines(buf, -1 , -1, False, example)
+            api.buf_set_lines(buf, -1 , -1, False, ["=================="])
+        api.buf_set_option(buf, 'modifiable', False)
+
+    def _show_example_windows(self):
+        api = self.nvim.api
+        buf = api.create_buf(False, True)
+        api.buf_set_option(buf, 'bufhidden', 'wipe')
+        width = api.get_option("columns")
+        height = api.get_option("lines")
+        win_height = math.ceil(height * 0.8 - 4)
+        win_width = math.ceil(width * 0.8)
+        row = math.ceil((height - win_height) / 2 - 1)
+        col = math.ceil((width - win_width) / 2)
+        opts = {
+          "style" : "minimal",
+          "relative" : "editor",
+          "width" : win_width,
+          "height" : win_height,
+          "row" : row,
+          "col" : col
+        }
+        win = api.open_win(buf, True, opts)
+        return buf
 
     def _get_resource_url(self, resource_name: str):
         provider_url = "https://registry.terraform.io/v2/provider-docs"
         params = {
-            "filter[provider-version]": 27638, # hard code provide-version
-            "filter[category]": "resources", # hard ccode provide resource
+            "filter[provider-version]": 27638,  # hard code provide-version
+            "filter[category]": "resources",  # hard ccode provide resource
             "filter[slug]": resource_name,
         }
 
-        res = requests.get(provider_url, params = params)
+        self.log(provider_url)
+        res = requests.get(provider_url, params=params)
 
-        resource_url = res.json()['data'][0]['links']['self']
+        resource_url = res.json()["data"][0]["links"]["self"]
+        registry_url = "https://registry.terraform.io"
+        return registry_url + resource_url
 
-        return resource_url
+    def log(self, data):
+        with open('/tmp/log.txt', "a") as f:
+            f.writelines("\n" + str(data) + "\n") 
